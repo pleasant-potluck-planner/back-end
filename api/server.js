18c8c097d94c07be46ce3e0fb642a209f543c985
tdubs <contact-tdubs@protonmail.com>
2021-09-20 10:58:27 -0700

    Boilerplate ready to be deployed to Heroku

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const db = require('./data/db-config')
+const morgan = require('morgan')
 
 function getAllUsers() { return db('users') }
 
@@ -17,6 +18,7 @@ const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+server.use(morgan('dev'))
 
 server.get('/api/users', async (req, res) => {
   res.json(await getAllUsers())
