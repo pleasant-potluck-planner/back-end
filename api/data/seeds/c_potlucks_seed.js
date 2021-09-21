51e1bf22a3c55e54839f0c2cc7c60f4f3f9132e9
tdubs <contact-tdubs@protonmail.com>
2021-09-21 15:43:32 -0700

    Added potluck_id and user_id to seed files, as well as changed booleans to true/false values.

--- api/data/seeds/c_potlucks_seed.js
+++ api/data/seeds/c_potlucks_seed.js
@@ -1,29 +1,34 @@
 
 const potlucks_list = [{
+  potluck_id: 1,
   potluck_name: "John's potluck",
   potluck_location: "John's house",
   potluck_date: "02-10-2022",
   potluck_time: "18:00",
   user_id: 1
 },{
+  potluck_id: 2,
   potluck_name: "Joe's birthday",
   potluck_location: "the big venue",
   potluck_date: "03-22-2022",
   potluck_time: "8:00",
   user_id: 2
 },{
+  potluck_id: 3,
   potluck_name: "John's potluck",
   potluck_location: 1234,
   potluck_date: "02-17-2022",
   potluck_time: "20:00",
   user_id: 1
 },{
+  potluck_id: 4,
   potluck_name: "Surprise!",
   potluck_location: "sshhuut... this is a secret",
   potluck_date: "03-03-2022",
   potluck_time: "21:00",
   user_id: 3
 },{
+  potluck_id: 5,
   potluck_name: "baz is back!",
   potluck_location: "gate 21 hall B",
   potluck_date: "1-1-2021",
